65f52ed34c8a026f85d1c41d82348d0298a45fdf
adsbvb <adsbvb@yandex.ru>
2026-01-02 13:13:57 +0500

    feat: correct users dto

--- main-service/src/main/java/ru/practicum/service/dto/NewUserRequest.java
+++ main-service/src/main/java/ru/practicum/service/dto/NewUserRequest.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class NewUserRequest {
     @NotBlank(message = "Электронный адрес должен быть указан")
     @Email(message = "Неверный формат электронного адреса")
