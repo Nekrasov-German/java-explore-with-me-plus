357f03865e3c6bfd1505c23c5586c6479206193a
adsbvb <adsbvb@yandex.ru>
2026-01-04 04:26:40 +0500

    fix: add new Enums, correct UpdateEventDtos

--- main-service/src/main/java/ru/practicum/service/dto/UpdateEventAdminRequest.java
+++ main-service/src/main/java/ru/practicum/service/dto/UpdateEventAdminRequest.java
@@ -2,22 +2,29 @@ package ru.practicum.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
-import lombok.Data;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.service.model.enums.State;
+import ru.practicum.service.dto.enums.AdminStateAction;
+import ru.practicum.service.error.ValidationException;
 
 import java.time.LocalDateTime;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateEventAdminRequest {
-    // Данные для изменения информации о событии. Если поле в запросе не указано (равно null) - значит изменение этих данных не треубется.
+    // Данные для изменения информации о событии. Если поле в запросе не указано (равно null) - значит изменение этих данных не требуется.
+    @Size(min = 20, max = 2000)
     String annotation;
 
     @JsonProperty(value = "category")
     Long categoryId;
 
+    @Size(min = 20, max = 7000)
     String description;
 
     @JsonFormat(pattern = Constant.DATE_TIME_FORMAT)
@@ -32,7 +39,16 @@ public class UpdateEventAdminRequest {
 
     Boolean requestModeration;
 
-    State state;
+    AdminStateAction stateAction;
 
+    @Size(min = 3, max = 120)
     String title;
+
+    /*@AssertTrue(message = "Дата начала события должна быть не ранее чем за час от даты публикации")
+    public boolean isValidEventDate() {
+        if (eventDate == null) {
+            return true;
+        }
+        return eventDate.isBefore(LocalDateTime.now().plusHours(1));
+    }*/
 }
