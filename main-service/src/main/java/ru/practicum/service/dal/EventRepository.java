3ad171076a7b239779ec60adb4bf7d11c7572019
adsbvb <adsbvb@yandex.ru>
2026-01-04 00:35:03 +0500

    feat: add method EventRepository

--- main-service/src/main/java/ru/practicum/service/dal/EventRepository.java
+++ main-service/src/main/java/ru/practicum/service/dal/EventRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.practicum.service.model.Event;
+import ru.practicum.service.model.enums.State;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -44,4 +45,19 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAllByIdIn(Set<Long> ids);
 
     Page<Event> findByInitiator_Id(Long userId, Pageable pageable);
+
+    @Query("SELECT e FROM Event e " +
+            "WHERE (:users IS NULL OR e.initiator.id IN :users) " +
+            "AND (:states IS NULL OR e.state IN :states) " +
+            "AND (:categories IS NULL OR e.category.id IN :categories) " +
+            "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
+            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd) " +
+            "ORDER BY e.createdOn DESC")
+    List<Event> findEventByAdmin(
+            @Param("users") List<Long> users,
+            @Param("states") List<State> states,
+            @Param("categories") List<Long> categories,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
+            Pageable pageable);
 }
